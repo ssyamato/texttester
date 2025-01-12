1df0b236a997c88f2ff91d3cdbad499381cecdd4
ssyamato <ss.yamato.dev@gmail.com>
2025-01-12 21:59:19 +0900

    処理をREST-API化

--- textmining.py
+++ textmining.py
@@ -9,16 +9,23 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import ipadic
 import re
+# 起動時に引数を受け取る
+import sys
+# ファイル名にタイムスタンプを付ける
+import datetime
+import calendar
+# REST API 化のためのライブラリ
+from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+import uvicorn
+from pydantic import BaseModel 
 
-# 青空文庫「吾輩は猫である」のURL
-url = "https://times.abema.tv/fifaworldcup/articles/-/10032462"
-response = requests.get(url)
-# 「BeautifulSoup」でサイトの情報を操作できる。
-# 　ここではテキストのデータのみ抜き出す。
-soup = BeautifulSoup(response.content, "html.parser")
-text = soup.get_text()
+# API化のためのライブラリ
+app = FastAPI()
 
-#関数の設定
+#テキストマイニングの関数
 def mecab_tokenizer(text):
     replaced_text = unicodedata.normalize("NFKC",text)
     replaced_text = replaced_text.upper()
@@ -51,28 +58,59 @@ def mecab_tokenizer(text):
     #各トークンを少しスペースを空けて（' '）結合
     return ' '.join(token_list)
 
-#関数の実行
-words = mecab_tokenizer(text)
+# ▽＝＝＝＝サンプル＝＝＝＝▽
+# 青空文庫「吾輩は猫である」のURL
+#url = "https://times.abema.tv/fifaworldcup/articles/-/10032462"
+#response = requests.get(url)
+# 「BeautifulSoup」でサイトの情報を操作できる。
+# 　ここではテキストのデータのみ抜き出す。
+#soup = BeautifulSoup(response.content, "html.parser")
+#text = soup.get_text()
+# △＝＝＝＝サンプル＝＝＝＝△
 
-#（大和修正）大和のローカル環境に合わせる
-# フォントの設定
-font_path=r"C:\Windows\Fonts\HGRPP1.TTC"
-#色の設定
-colormap="Paired"
+#アプリケーションでエラーが発生した際に呼び出す関数
+@app.exception_handler(RequestValidationError)
+async def handler(request:Request, exc:RequestValidationError):
+    print(exc)
+    return JSONResponse(content={}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
-wordcloud = WordCloud(
-    background_color="white",
-    width=800,
-    height=800,
-    font_path=font_path,
-    colormap = colormap,
-    stopwords=["する", "ある", "こと", "ない"],
-    max_words=100,
-    ).generate(words)
+# 1項目しか受け取り予定はないが、いったん受け口となるBeanを作成
+class Text(BaseModel):
+    text: str
 
-plt.figure(figsize=(10, 10))
-plt.imshow(wordcloud, interpolation="bilinear")
-plt.axis("off")
-# ファイルを保存
-plt.savefig('sample.png')
-plt.show()
+@app.post("/create/")
+async def create_image(text: Text):
+
+    #関数の実行
+    words = mecab_tokenizer(text.text)
+
+    #（大和修正）大和のローカル環境に合わせる
+    # フォントの設定
+    font_path=r"C:\Windows\Fonts\HGRPP1.TTC"
+    #色の設定
+    colormap="Paired"
+
+    wordcloud = WordCloud(
+        background_color="white",
+        width=800,
+        height=800,
+        font_path=font_path,
+        colormap = colormap,
+        stopwords=["する", "ある", "こと", "ない"],
+        max_words=100,
+        ).generate(words)
+
+    plt.figure(figsize=(10, 10))
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    # ファイルを保存
+    now = datetime.datetime.now()
+    filename = 'textmining_' + now.strftime('%Y%m%d_%H%M%S') + '.png'
+    plt.savefig(filename)
+    return filename
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000, log_level="debug")
+
+    #サンプルコマンド
+    #curl -X POST "http://localhost:8000/create/" -H "Content-Type: application/json" -d "{\"text\":\"小説家・翻訳家・評論家。1903（明治36）年、東京牛込に生まれる。一高理科に入学し、堀辰雄を知って文学への関心が高まり、東京外国語露語科に転学。卒業後、北大図書館に勤めながら創作を志し、ソ連通商部を経たあと文筆生活に入る。ロシア語はもとよりフランス語にも長け、プーシキン、ツルゲーネフ、ガルシン、チェーホフ、ゴーリキー～バルザック、ジード、シャルドンヌらの諸作品を翻訳する。\"}"
